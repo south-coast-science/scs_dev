08b7441e22f5066778fbdc94f575d9d1708fdc69
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-01-21 11:01:03 +0000

    Added opc_power toop-level script.

---
+++ scs_dev/cmd/cmd_opc_power.py
@@ -0,0 +1,64 @@
+"""
+Created on 21 Jan 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import optparse
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CmdOPCPower(object):
+    """unix command line handler"""
+
+    def __init__(self, default_interval=1):
+        """stuff"""
+        self.__parser = optparse.OptionParser(usage="%prog 1 | 0 [-v]", version="%prog 1.0")
+
+        # optional...
+        self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False, help="report narrative to stderr")
+
+        self.__opts, self.__args = self.__parser.parse_args()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def is_valid(self):
+        if self.power is None:
+            return False
+
+        return True
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def power(self):
+        try:
+            return bool(int(self.__args[0]))
+        except RuntimeError:
+            return None
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def verbose(self):
+        return self.__opts.verbose
+
+
+    @property
+    def args(self):
+        return self.__args
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def print_help(self, file):
+        self.__parser.print_help(file)
+
+
+    def __str__(self, *args, **kwargs):
+        return "CmdOPCPower:{power:%d, verbose:%s, args:%s}" % \
+                    (self.power, self.verbose, self.args)
---
+++ scs_dev/opc_power.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+"""
+Created on 21 Jan 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+command line example:
+./scs_dev/opc_power.py 1
+"""
+
+import sys
+
+from scs_core.data.json import JSONify
+from scs_core.sys.exception_report import ExceptionReport
+
+from scs_dev.cmd.cmd_opc_power import CmdOPCPower
+
+from scs_dfe.particulate.opc_n2 import OPCN2
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    sckt = None
+    sender = None
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # cmd...
+
+    cmd = CmdOPCPower()
+
+    if not cmd.is_valid():
+        cmd.print_help(sys.stderr)
+        exit()
+
+    if cmd.verbose:
+        print(cmd, file=sys.stderr)
+
+    try:
+        # ------------------------------------------------------------------------------------------------------------
+        # resource...
+
+        opc = OPCN2()
+
+        if cmd.verbose:
+            print(opc, file=sys.stderr)
+
+
+        # ------------------------------------------------------------------------------------------------------------
+        # run...
+
+        if cmd.power:
+            opc.on()
+        else:
+            opc.off()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # end...
+
+    except KeyboardInterrupt as ex:
+        if cmd.verbose:
+            print("opc_power: KeyboardInterrupt", file=sys.stderr)
+
+    except Exception as ex:
+        print(JSONify.dumps(ExceptionReport.construct(ex)), file=sys.stderr)
