b1322da04ac6dcb5207ffc558a3e34ea17471546
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-24 08:21:01 +0100

    Moved gas data structures from scs_dfe to scs_core.

--- tests/dfe/gasses_test.py
+++ tests/dfe/gasses_test.py
@@ -9,14 +9,17 @@ Created on 20 Oct 2016
 import sys
 
 from scs_core.data.json import JSONify
+
+from scs_core.gas.afe_baseline import AFEBaseline
+from scs_core.gas.afe_calib import AFECalib
+from scs_core.gas.pt1000_calib import Pt1000Calib
+
 from scs_core.sys.system_id import SystemID
 
 from scs_dev.sampler.gases_sampler import GasesSampler
 
 from scs_dfe.climate.sht_conf import SHTConf
-from scs_dfe.gas.afe_baseline import AFEBaseline
-from scs_dfe.gas.afe_calib import AFECalib
-from scs_dfe.gas.pt1000_calib import Pt1000Calib
+from scs_dfe.gas.pt1000 import Pt1000
 
 from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
@@ -35,7 +38,7 @@ sht_conf = SHTConf.load_from_host(Host)
 sht = sht_conf.int_sht()
 
 calib = Pt1000Calib.load_from_host(Host)
-pt1000 = calib.pt1000()
+pt1000 = Pt1000(calib)
 
 afe_baseline = AFEBaseline.load_from_host(Host)
 
