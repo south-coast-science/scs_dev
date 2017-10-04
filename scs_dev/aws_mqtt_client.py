4854ef2c1d8eb66ebe55fc480a8adeabb34f3bc6
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-10-04 14:32:53 +0100

    Added AWS configuration classes.

--- scs_dev/aws_mqtt_client.py
+++ scs_dev/aws_mqtt_client.py
@@ -25,10 +25,15 @@ from collections import OrderedDict
 
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
+from scs_core.aws.client.client_id import ClientID
+from scs_core.aws.service.endpoint import Endpoint
+
 from scs_core.data.json import JSONify
 from scs_core.data.localized_datetime import LocalizedDatetime
 
 from scs_core.sys.exception_report import ExceptionReport
+
+from scs_host.sys.host import Host
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -87,14 +92,14 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------------------------------------------------
     # cmd...
 
-    host = "asrfh6e5j5ecz.iot.us-west-2.amazonaws.com"
-    rootCAPath = "/home/pi/SCS/aws/root-CA.crt"
-    certificatePath = "/home/pi/SCS/aws/scs-rpi-006.cert.pem"  # 9f01402232-certificate.pem.crt
-    privateKeyPath = "/home/pi/SCS/aws/scs-rpi-006.private.key"  # 9f01402232-private.pem.key
+    cert_dir = Host.aws_dir() + 'cert/'
+
+    rootCAPath = cert_dir + "root-CA.crt"
+    certificatePath = cert_dir + "scs-rpi-006.cert.pem"  # 9f01402232-certificate.pem.crt
+    privateKeyPath = cert_dir + "scs-rpi-006.private.key"  # 9f01402232-private.pem.key
     clientId = "rpi-006"
     topic = "bruno/1"
 
-    # host = "asrfh6e5j5ecz.iot.us-west-2.amazonaws.com"
     # rootCAPath = "/home/pi/SCS/aws/root-CA.crt"
     # certificatePath = "/home/pi/SCS/aws/scs-rpi-006.cert.pem"  # 9f01402232-certificate.pem.crt
     # privateKeyPath = "/home/pi/SCS/aws/scs-rpi-006.private.key"  # 9f01402232-private.pem.key
@@ -104,6 +109,15 @@ if __name__ == '__main__':
     try:
         # ------------------------------------------------------------------------------------------------------------
         # resources...
+
+        endpoint = Endpoint.load_from_host(Host)
+
+
+        client_id = ClientID.load_from_host(Host)
+
+        root_ca_file_path = client_id.root_ca_file_path(Host)
+        private_key_path = client_id.private_key_path(Host)
+        certificate_path = client_id.certificate_path(Host)
 
         logger = logging.getLogger("AWSIoTPythonSDK.core")
         logger.setLevel(logging.DEBUG)
@@ -116,8 +130,8 @@ if __name__ == '__main__':
 
         myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
 
-        myAWSIoTMQTTClient.configureEndpoint(host, 8883)
-        myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+        myAWSIoTMQTTClient.configureEndpoint(endpoint.host, 8883)
+        myAWSIoTMQTTClient.configureCredentials(root_ca_file_path, private_key_path, certificate_path)
 
         myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
         myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
