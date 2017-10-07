03e81e5777a2bc3010cdaf3afe73ff449cd42fe0
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-10-07 10:29:02 +0100

    Added aws_topic_subscriber top-level script.

---
+++ scs_dev/aws_topic_subscriber.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+"""
+Created on 7 Oct 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+https://opensensorsio.helpscoutdocs.com/article/84-overriding-timestamp-information-in-message-payload
+
+Requires SystemID and Project documents.
+
+command line example:
+./aws_mqtt_client.py south-coast-science-dev/development/device/alpha-bb-eng-000003/control | \
+./aws_topic_subscriber.py -t south-coast-science-dev/development/device/alpha-bb-eng-000003/control
+"""
+
+import json
+import sys
+
+from collections import OrderedDict
+
+from scs_core.data.json import JSONify
+from scs_core.data.publication import Publication
+
+from scs_core.osio.config.project import Project
+
+from scs_core.sys.system_id import SystemID
+from scs_core.sys.exception_report import ExceptionReport
+
+from scs_dev.cmd.cmd_aws_topic_subscriber import CmdAWSTopicSubscriber
+
+from scs_host.sys.host import Host
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # cmd...
+
+    cmd = CmdAWSTopicSubscriber()
+
+    if not cmd.is_valid():
+        cmd.print_help(sys.stderr)
+        exit(2)
+
+    if cmd.verbose:
+        print(cmd, file=sys.stderr)
+
+    try:
+        # ------------------------------------------------------------------------------------------------------------
+        # resources...
+
+        # topic...
+        if cmd.channel:
+            # SystemID...
+            system_id = SystemID.load(Host)
+
+            if system_id is None:
+                print("SystemID not available.", file=sys.stderr)
+                exit(1)
+
+            if cmd.verbose:
+                print(system_id, file=sys.stderr)
+
+            # Project...
+            project = Project.load(Host)        # TODO: replace with AWS project
+
+            if project is None:
+                print("Project not available.", file=sys.stderr)
+                exit(1)
+
+            topic = project.channel_path(cmd.channel, system_id)
+
+        else:
+            topic = cmd.topic
+
+        if cmd.verbose:
+            print(topic, file=sys.stderr)
+            sys.stderr.flush()
+
+
+        # ------------------------------------------------------------------------------------------------------------
+        # run...
+
+        for line in sys.stdin:
+            try:
+                jdict = json.loads(line, object_pairs_hook=OrderedDict)
+            except ValueError:
+                continue
+
+            publication = Publication.construct_from_jdict(jdict)
+
+            if publication.topic == topic:
+                print(JSONify.dumps(publication.payload))
+                sys.stdout.flush()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # end...
+
+    except KeyboardInterrupt:
+        if cmd.verbose:
+            print("aws_topic_subscriber: KeyboardInterrupt", file=sys.stderr)
+
+    except Exception as ex:
+        print(JSONify.dumps(ExceptionReport.construct(ex)), file=sys.stderr)
---
+++ scs_dev/cmd/cmd_aws_topic_subscriber.py
@@ -0,0 +1,80 @@
+"""
+Created on 19 Nov 2016
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import optparse
+
+from scs_core.osio.config.project import Project        # TODO: replace with AWS project
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CmdAWSTopicSubscriber(object):
+    """unix command line handler"""
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.__parser = optparse.OptionParser(usage="%prog { -t TOPIC | -c { C | G | P | S | X } } [-v]",
+                                              version="%prog 1.0")
+
+        # compulsory...
+        self.__parser.add_option("--topic", "-t", type="string", nargs=1, action="store", dest="topic",
+                                 help="topic path")
+
+        self.__parser.add_option("--channel", "-c", type="string", nargs=1, action="store", dest="channel",
+                                 help="publication channel")
+
+        # optional...
+        self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False,
+                                 help="report narrative to stderr")
+
+        self.__opts, self.__args = self.__parser.parse_args()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def is_valid(self):
+        if bool(self.topic) == bool(self.channel):
+            return False
+
+        if self.channel and not Project.is_valid_channel(self.channel):
+            return False
+
+        return True
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def topic(self):
+        return self.__opts.topic
+
+
+    @property
+    def channel(self):
+        return self.__opts.channel
+
+
+    @property
+    def verbose(self):
+        return self.__opts.verbose
+
+
+    @property
+    def args(self):
+        return self.__args
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def print_help(self, file):
+        self.__parser.print_help(file)
+
+
+    def __str__(self, *args, **kwargs):
+        return "CmdAWSTopicSubscriber:{topic:%s, channel:%s, verbose:%s, args:%s}" % \
+                    (self.topic, self.channel, self.verbose, self.args)
