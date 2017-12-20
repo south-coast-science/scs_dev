bfbc48dd83a9abeb9f640ba0078069d6ffc1776e
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-12-20 16:35:56 +0000

    Cleaning.

--- tests/dfe/gasses_test.py
+++ tests/dfe/gasses_test.py
@@ -11,11 +11,15 @@ import sys
 from scs_core.data.json import JSONify
 from scs_core.sync.timed_runner import TimedRunner
 from scs_core.sys.system_id import SystemID
+
 from scs_dev.sampler.gases_sampler import GasesSampler
+
 from scs_dfe.climate.sht_conf import SHTConf
 from scs_dfe.gas.afe_conf import AFEConf
+
 from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
+
 from scs_ndir.gas.ndir import NDIR
 
 
