0baa9468a210d4a0bfd23f5a70b80b5ce5459b1e
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-06-21 16:07:29 +0100

    Removed null host temperatures from SystemTemp document.

--- src/scs_dev/status_sampler.py
+++ src/scs_dev/status_sampler.py
@@ -42,12 +42,12 @@ FILES
 ~/SCS/conf/system_id.json
 
 DOCUMENT EXAMPLE - OUTPUT
-{"tag": "scs-ap1-6", "rec": "2018-04-05T14:17:01.943+00:00",
-"val": {"tz": {"name": "Europe/London", "utc-offset": "+01:00"},
-"sch": {"scs-climate": {"interval": 60.0, "tally": 1}, "scs-gases": {"interval": 5.0, "tally": 1},
-"scs-particulates": {"interval": 10.0, "tally": 1}, "scs-status": {"interval": 60.0, "tally": 1}},
-"tmp": {"brd": 27.6, "hst": 42.9}, "up": {"period": "00-00:08:00.000",
-"users": 3, "load": {"av1": 0.0, "av5": 0.09, "av15": 0.08}}}}
+{"tag": "scs-be2-3", "rec": "2018-06-21T15:03:02.875+00:00", "val": {
+"tz": {"name": "Etc/UCT", "utc-offset": "+00:00"},
+"pos": {"lat": 50.823049, "lng": -0.123024, "alt": 48, "qual": 2},
+"sch": {"scs-climate": {"interval": 60.0, "tally": 1}, "scs-gases": {"interval": 10.0, "tally": 1},
+"scs-status": {"interval": 60.0, "tally": 1}}, "tmp": {"brd": 31.2},
+"up": {"period": "01-05:29:00.000", "users": 2, "load": {"av1": 0.04, "av5": 0.01, "av15": 0.0}}}}
 
 SEE ALSO
 scs_dev/scheduler
