1a146dce24aef968030e7a6e2292360f7b7c21a8
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-07-07 10:28:26 +0100

    Removed TODO.

--- src/scs_dev/aws_mqtt_client.py
+++ src/scs_dev/aws_mqtt_client.py
@@ -73,8 +73,6 @@ from scs_host.comms.stdio import StdIO
 from scs_host.sys.host import Host
 
 
-# TODO: do not use the queue - publish directly?
-
 # --------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
