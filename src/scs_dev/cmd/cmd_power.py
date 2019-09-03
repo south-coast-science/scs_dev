0d8956d9032960f5aa844e4237e7c440b44dd925
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-09-03 11:48:19 +0100

    Upgraded interface_power.py

--- src/scs_dev/cmd/cmd_power.py
+++ src/scs_dev/cmd/cmd_power.py
@@ -2,6 +2,13 @@
 Created on 21 Jan 2017
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+    def power_gases(self, enable):                  # switches digital component only
+    def power_gps(self, enable):
+    def power_modem(self, enable):
+    def power_ndir(self, enable):
+    def power_opc(self, enable):
+        pass
+
 """
 
 import optparse
@@ -12,13 +19,52 @@ import optparse
 class CmdPower(object):
     """unix command line handler"""
 
+
+    @classmethod
+    def is_valid_boolean(cls, value):
+        if value is None:
+            return True
+
+        try:
+            boolean = int(value)
+            return boolean == 0 or boolean == 1
+        except (TypeError, ValueError):
+            return False
+
+
+    @classmethod
+    def boolean(cls, value):
+        try:
+            return bool(int(value))
+        except (TypeError, ValueError):
+            return None
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
     def __init__(self):
         """
         Constructor
         """
-        self.__parser = optparse.OptionParser(usage="%prog { 1 | 0 } [-v]", version="%prog 1.0")
+        self.__parser = optparse.OptionParser(usage="%prog { [-g ENABLE] [-p ENABLE] [-m ENABLE] [-n ENABLE] "
+                                                    "[-o ENABLE] | ENABLE_ALL } [-v]", version="%prog 1.0")
 
         # optional...
+        self.__parser.add_option("--gases", "-g", type="int", nargs=1, action="store", dest="gases",
+                                 help="switch gas sensors ON (1) or OFF (0)")
+
+        self.__parser.add_option("--gps", "-p", type="int", nargs=1, action="store", dest="gps",
+                                 help="switch GPS receiver ON (1) or OFF (0)")
+
+        self.__parser.add_option("--modem", "-m", type="int", nargs=1, action="store", dest="modem",
+                                 help="switch modem ON (1) or OFF (0)")
+
+        self.__parser.add_option("--ndir", "-n", type="int", nargs=1, action="store", dest="ndir",
+                                 help="switch NDIR sensor ON (1) or OFF (0)")
+
+        self.__parser.add_option("--opc", "-o", type="int", nargs=1, action="store", dest="opc",
+                                 help="switch particulate sensor ON (1) or OFF (0)")
+
         self.__parser.add_option("--verbose", "-v", action="store_true", dest="verbose", default=False,
                                  help="report narrative to stderr")
 
@@ -28,7 +74,16 @@ class CmdPower(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     def is_valid(self):
-        if self.power is None:
+        if not self.is_valid_boolean(self.__opts.gases) or not self.is_valid_boolean(self.__opts.gps) or \
+                not self.is_valid_boolean(self.__opts.modem) or not self.is_valid_boolean(self.__opts.ndir) or \
+                not self.is_valid_boolean(self.__opts.opc):
+            return False
+
+        if len(self.__args) > 0 and not self.is_valid_boolean(self.__args[0]):
+            return False
+
+        if self.all is not None and (self.gases is not None or self.gps is not None or self.modem is not None or
+                                     self.ndir is not None or self.opc is not None):
             return False
 
         return True
@@ -37,17 +92,34 @@ class CmdPower(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     @property
-    def power(self):
-        if len(self.__args) > 0:
-            try:
-                return bool(int(self.__args[0]))
-            except RuntimeError:
-                return None
-
-        return None
+    def gases(self):
+        return self.boolean(self.__opts.gases)
 
 
-    # ----------------------------------------------------------------------------------------------------------------
+    @property
+    def gps(self):
+        return self.boolean(self.__opts.gps)
+
+
+    @property
+    def modem(self):
+        return self.boolean(self.__opts.modem)
+
+
+    @property
+    def ndir(self):
+        return self.boolean(self.__opts.ndir)
+
+
+    @property
+    def opc(self):
+        return self.boolean(self.__opts.opc)
+
+
+    @property
+    def all(self):
+        return self.boolean(self.__args[0]) if len(self.__args) > 0 else None
+
 
     @property
     def verbose(self):
@@ -61,4 +133,5 @@ class CmdPower(object):
 
 
     def __str__(self, *args, **kwargs):
-        return "CmdPower:{power:%d, verbose:%s}" % (self.power, self.verbose)
+        return "CmdPower:{gases:%s, gps:%s, modem:%s, ndir:%s, opc:%s, all:%s, verbose:%s}" % \
+               (self.gases, self.gps, self.modem, self.ndir, self.opc, self.all, self.verbose)
--- src/scs_dev/interface_power.py
+++ src/scs_dev/interface_power.py
@@ -13,13 +13,10 @@ For other DFE boards - such as that for Raspberry Pi - the utility is only able 
 the NDIR and OPC.
 
 SYNOPSIS
-interface_power.py { 1 | 0 } [-v]
+interface_power.py { [-g ENABLE] [-p ENABLE] [-m ENABLE] [-n ENABLE] [-o ENABLE] | ENABLE_ALL } [-v]
 
 EXAMPLES
 ./interface_power.py 0
-
-SEE ALSO
-scs_dev/opc_power
 """
 
 import sys
@@ -28,8 +25,8 @@ from scs_core.sys.signalled_exit import SignalledExit
 
 from scs_dev.cmd.cmd_power import CmdPower
 
+from scs_dfe.gps.gps_conf import GPSConf
 from scs_dfe.interface.interface_conf import InterfaceConf
-
 from scs_dfe.particulate.opc_conf import OPCConf
 
 from scs_host.bus.i2c import I2C
@@ -39,6 +36,40 @@ try:
     from scs_ndir.gas.ndir_conf import NDIRConf
 except ImportError:
     from scs_core.gas.ndir_conf import NDIRConf
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+def power_gps(enable):
+    if gps is None:
+        return
+
+    if enable:
+        gps.power_on()
+    else:
+        gps.power_off()
+
+
+def power_ndir(enable):
+    if ndir is None:
+        return
+
+    if enable:
+        ndir.power_on()
+    else:
+        ndir.power_off()
+
+
+def power_opc(enable):
+    if opc is None:
+        return
+
+    if enable:
+        opc.power_on()
+        opc.operations_on()
+    else:
+        # opc.operations_off()
+        opc.power_off()
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -79,6 +110,13 @@ if __name__ == '__main__':
             print("interface_power: %s" % interface, file=sys.stderr)
             sys.stderr.flush()
 
+        # GPS...
+        gps_conf = GPSConf.load(Host)
+        gps = None if gps_conf is None else gps_conf.gps(interface, Host)
+
+        if cmd.verbose and gps_conf:
+            print("interface_power: %s" % gps_conf, file=sys.stderr)
+
         # OPC...
         opc_conf = OPCConf.load(Host)
         opc = None if opc_conf is None else opc_conf.opc(interface, Host)
@@ -97,25 +135,28 @@ if __name__ == '__main__':
         # ------------------------------------------------------------------------------------------------------------
         # run...
 
-        if cmd.power:
-            # Interface...
-            interface.power_sensors(True)
+        if cmd.all is not None:
+            interface.power_gases(cmd.all)
+            power_gps(cmd.all)
+            power_ndir(cmd.all)
+            power_opc(cmd.all)
+
             # io.led_red = True             # TODO: fix LED control
-            # io.led_green = True
 
-        else:
-            # OPC...
-            if opc:
-                opc.operations_off()         # needed because some DFEs do not have power control
+        if cmd.gases is not None:
+            interface.power_gases(cmd.gases)
 
-            # NDIR...
-            if ndir:
-                ndir.lamp_run(False)         # needed because some DFEs do not have power control
+        if cmd.gps is not None:
+            power_gps(cmd.gps)
 
-            # Interface...
-            interface.power_sensors(False)
-            # io.led_red = False
-            # io.led_green = False
+        if cmd.modem is not None:
+            interface.power_modem(cmd.modem)
+
+        if cmd.ndir is not None:
+            power_ndir(cmd.ndir)
+
+        if cmd.opc is not None:
+            power_opc(cmd.opc)
 
 
     # ----------------------------------------------------------------------------------------------------------------
