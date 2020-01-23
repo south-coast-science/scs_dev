6cce65dcf78926986ea397623c12e3514ea1c766
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-01-23 17:22:34 +0000

    Updated documentation

--- src/scs_dev/csv_log_sync.py
+++ src/scs_dev/csv_log_sync.py
@@ -8,14 +8,12 @@ Created on 16 Jan 2020
 DESCRIPTION
 The csv_log_sync utility is used to extract JSON found from the CSV files created by the csv_logger utility.
 
-The utility is designed to be invoked of device start-up. At this point, csv_log_sync can interrogate the server
-in order to determine whether there are any found stored on the device with a more recent 'rec' datetime than the
-most recent document on the server. If so, these found can be published before device sensing operations begin.
+The utility can interrogate the server in order to determine whether there are any found stored on the device with a
+more recent 'rec' datetime than the most recent document on the server. If so, these found can be published before
+device sensing operations begin. An alternate test mode is provided in which a single topic and start datetime is
+provided on the command line.
 
-Documents are found in order of messaging topic, then datetime, with the oldest found found first.
-
-In a typical setting, topic paths and start datetimes are found automatically from the server, an alternate test mode
-is provided in which a single topic and start datetime is provided on the command line.
+Documents are found in order of datetime, with the oldest found found first.
 
 In order to interrogate the server, both an AWS API authentication configuration and system ID must be set. The
 csv_log_sync utility is not supported by the Open Sensors data platform.
@@ -24,10 +22,10 @@ Note that the csv_log_sync utility is only able to find missing server found at 
 it is not able to infill gaps within the server data timeline.
 
 SYNOPSIS
-csv_log_sync.py { -t TOPIC_NAME -s START | -f } [-n] [-w] [-p UDS_PUB] [-v]
+csv_log_sync.py -t TOPIC_NAME { -s START | -f } [-n] [-v]
 
 EXAMPLES
-./csv_log_sync.py -vfw -p ~/SCS/pipes/mqtt_publication.uds
+./csv_log_sync.py -t climate -vf
 
 SEE ALSO
 scs_dev/csv_logger
