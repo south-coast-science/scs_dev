161f6ea2a9a1032576ea5f21edd0cc014964314b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-07-24 09:53:13 +0100

    Added uds_receiver.py

---
+++ src/scs_dev/uds_receiver.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+"""
+Created on 11 Apr 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+source repo: scs_analysis
+
+DESCRIPTION
+The uds_receiver utility is used to accept data via a Unix domain socket, with data sourced from the same host, or
+another host on the same local area network.
+
+SYNOPSIS
+uds_receiver.py [-v] UDS_SUB
+
+EXAMPLES
+./uds_receiver.py scs-particulates.uds
+
+SEE ALSO
+scs_analysis/socket_receiver
+"""
+
+import sys
+
+from scs_dev.cmd.cmd_uds import CmdUDS
+
+from scs_core.comms.uds_reader import UDSReader
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # cmd...
+
+    cmd = CmdUDS()
+
+    if not cmd.is_valid():
+        cmd.print_help(sys.stderr)
+        exit(2)
+
+    if cmd.verbose:
+        print("uds_receiver: %s" % cmd, file=sys.stderr)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # resources...
+
+    uds = UDSReader(cmd.path)
+
+    if cmd.verbose:
+        print("uds_receiver: %s" % uds, file=sys.stderr)
+        sys.stderr.flush()
+
+    try:
+        # ------------------------------------------------------------------------------------------------------------
+        # run...
+
+        uds.connect()
+
+        for message in uds.messages():
+            print(message)
+            sys.stdout.flush()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # end...
+
+    except KeyboardInterrupt:
+        if cmd.verbose:
+            print("uds_receiver: KeyboardInterrupt", file=sys.stderr)
+
+    finally:
+        uds.close()
